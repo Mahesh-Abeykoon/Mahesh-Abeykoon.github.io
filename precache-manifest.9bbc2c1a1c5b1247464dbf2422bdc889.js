051611aa6f29d2ad9d9cfe1cb6d5ebe3998b96e5
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-06-12 13:07:08 +0530

    Updates

---
+++ precache-manifest.9bbc2c1a1c5b1247464dbf2422bdc889.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480372d9c5160f61da817664305eb8e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ec106aab970cb23c99a",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "bab380ac6e9ed43cfcfc",
+    "url": "/static/js/2.c31209be.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c31209be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec106aab970cb23c99a",
+    "url": "/static/js/main.1345d8e5.chunk.js"
+  },
+  {
+    "revision": "37867d77d72cd394dad1",
+    "url": "/static/js/runtime-main.2763758a.js"
+  },
+  {
+    "revision": "598dcd88045ffe97d2ea7abb97f05cbb",
+    "url": "/static/media/profilepic.598dcd88.png"
+  },
+  {
+    "revision": "a0bc7d470698305dfbcf44b997e4b3c9",
+    "url": "/static/media/resume.a0bc7d47.pdf"
+  }
+]);
